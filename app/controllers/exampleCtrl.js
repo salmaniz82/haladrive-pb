5f23cc163fd2afd11535e1dbf544cf296c95f245
salmaniz82 <salmaniz.82@gmail.com>
2018-08-03 06:09:24 +0500

    filter UI and hooked to Model

--- app/controllers/exampleCtrl.js
+++ app/controllers/exampleCtrl.js
@@ -10,13 +10,89 @@
         var targetUrl = API_URL+'/api/vehicles/q/'+vm.queryString;
 
 
+        vm.mileRange = 500;
+        vm.priceRange = 800;
 
-        vm.appliedFilter = [];
 
 
-        vm.applyfilter = function()
+        $scope.appliedFilter = [];
+
+        $scope.appliedFilterItem = [];
+
+
+        $scope.filterHeader = function(filterHeaderSelc)
+        {
+            var arrIndex;
+            if($scope.appliedFilter.indexOf(filterHeaderSelc) == -1)
+            {
+                
+                $scope.appliedFilter.push(filterHeaderSelc);
+                return true;
+            }
+            else {
+                
+                var arrIndex = $scope.appliedFilter.indexOf(filterHeaderSelc);
+                $scope.appliedFilter.splice(arrIndex, 1); 
+                return false;
+            }
+           
+        }
+
+        $scope.filterHeaderClass = function(fh)
+        {
+           if ($scope.appliedFilter.indexOf(fh) == -1) 
+           {
+                return '';
+           }
+
+           else {
+                return 'CarBrowser__filters-list-open';
+           } 
+        }
+
+        $scope.isOpenList = function(fh)
         {
 
+           if ($scope.appliedFilter.indexOf(fh) == -1) 
+           {
+                return '';
+           }    
+           else {
+                return 'CarBrowser__filters-section-header-open open';
+           }
+
+        }
+
+
+        $scope.filterListSelc = function(item)
+        {
+
+            var arrIndex;
+            if($scope.appliedFilterItem.indexOf(item) == -1)
+            {
+                
+                $scope.appliedFilterItem.push(item);
+                return true;
+            }
+            else {
+                
+                var arrIndex = $scope.appliedFilterItem.indexOf(item);
+                $scope.appliedFilterItem.splice(arrIndex, 1); 
+                return false;
+            }
+
+        }
+
+        $scope.isFilterItemActive = function(item)
+        {
+            if($scope.appliedFilterItem.indexOf(item) == -1)
+            {
+                return '';
+            }
+            else {
+                return 'CarBrowser__filters-list-item-selected';
+            }
+            
         }
 
 
