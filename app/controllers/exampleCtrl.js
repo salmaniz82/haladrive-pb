1a710ed849e85a2cf55789646d2fc00ab5851e8e
salmaniz82 <salmaniz.82@gmail.com>
2018-08-04 03:38:56 +0500

    filter object mapping integrated

--- app/controllers/exampleCtrl.js
+++ app/controllers/exampleCtrl.js
@@ -7,7 +7,7 @@
         vm.cars = false;
         vm.queryString = '?all';
 
-        var targetUrl = API_URL+'/api/vehicles/q/'+vm.queryString;
+        vm.targetUrl = API_URL+'/api/vehicles/q/'+vm.queryString;
 
 
         vm.mileRange = 500;
@@ -15,89 +15,208 @@
 
 
 
-        $scope.appliedFilter = [];
+        $scope.appliedFilter = [ 
+
+            {'all': true}
+
+            
+        ];
 
         $scope.appliedFilterItem = [];
 
 
         $scope.filterHeader = function(filterHeaderSelc)
         {
-            var arrIndex;
-            if($scope.appliedFilter.indexOf(filterHeaderSelc) == -1)
-            {
-                
-                $scope.appliedFilter.push(filterHeaderSelc);
-                return true;
-            }
-            else {
-                
-                var arrIndex = $scope.appliedFilter.indexOf(filterHeaderSelc);
-                $scope.appliedFilter.splice(arrIndex, 1); 
-                return false;
-            }
-           
-        }
+            
+
+            var result = null;
+            var keyPointer = null;
+           for(var key in $scope.appliedFilter)
+           {
+                if($scope.appliedFilter[key].hasOwnProperty(filterHeaderSelc))
+                {
+                    result = true;
+
+                    keyPointer = key;
+                }
+                else {                   
+                    result = false;
+                }
+
+
+           }
+
+                if(!result)
+                {
+                    var temp = {};
+                    temp[filterHeaderSelc] = null;
+                    $scope.appliedFilter.push(temp);
+                }
+
+                else {
+                    $scope.appliedFilter.splice(keyPointer, 1);
+                }
+
+            
+
+        };
+
+
+        
 
         $scope.filterHeaderClass = function(fh)
         {
-           if ($scope.appliedFilter.indexOf(fh) == -1) 
+           
+           // 
+           var result = null;
+           for(var key in $scope.appliedFilter)
            {
-                return '';
+                if($scope.appliedFilter[key].hasOwnProperty(fh))
+                {
+                    result = true;
+                    keyPointer = key;
+                }
+                else {                   
+                    result = false;
+                }
            }
+                if(!result)
+                {
+                      return '';
+                }
+                else {
+                    return 'CarBrowser__filters-list-open';                   
+                }
 
-           else {
-                return 'CarBrowser__filters-list-open';
-           } 
         }
 
         $scope.isOpenList = function(fh)
         {
+            // 
 
-           if ($scope.appliedFilter.indexOf(fh) == -1) 
+            var result = null;
+           for(var key in $scope.appliedFilter)
            {
-                return '';
-           }    
-           else {
-                return 'CarBrowser__filters-section-header-open open';
+                if($scope.appliedFilter[key].hasOwnProperty(fh))
+                {
+                    result = true;
+                    keyPointer = key;
+                }
+                else {                   
+                    result = false;
+                }
            }
+                if(!result)
+                {
+                      return '';
+                }
+                else {
+                    return 'CarBrowser__filters-section-header-open open';
+                }
+
+           
 
         }
 
 
-        $scope.filterListSelc = function(item)
+        $scope.filterListSelc = function(itemObj)
         {
 
             var arrIndex;
-            if($scope.appliedFilterItem.indexOf(item) == -1)
-            {
+            var result = null;
+            var keyPointer = null;
+            var headerSlug = itemObj.g_slug;
+            headerSlug.toString();
+            var filterEntry = itemObj.titleEN;
+            var headerPosKey;
+            var headerMatched;
+            var stats;
+            /*
+                appliedFilterItem
+            */
+           for(var key in $scope.appliedFilterItem)
+           {
+                if( $scope.appliedFilterItem[key].hasOwnProperty(headerSlug) )
+                {
+                    headerMatched = true;
+                    headerPosKey = key;
+                    break;
+                }
+                else {  
+
+                    headerPosKey = key;
+
+                    if($scope.appliedFilterItem[headerPosKey][headerSlug] == undefined)
+                    {
+                        console.log($scope.appliedFilterItem[headerPosKey][headerSlug]);
+                        headerMatched = false;
+                    }
                 
-                $scope.appliedFilterItem.push(item);
-                return true;
-            }
-            else {
-                
-                var arrIndex = $scope.appliedFilterItem.indexOf(item);
-                $scope.appliedFilterItem.splice(arrIndex, 1); 
-                return false;
-            }
+                }
+           }
+
+                if(!headerMatched)
+                {
+   
+                    var temp = {};
+                    temp[headerSlug] = [filterEntry];
+                    $scope.appliedFilterItem.push(temp);
+                    
+                }
+
+                else {
+                    if($scope.appliedFilterItem[headerPosKey][headerSlug].indexOf(filterEntry) != -1)
+                    {
+                        var idx = $scope.appliedFilterItem[headerPosKey][headerSlug].indexOf(filterEntry);
+                        $scope.appliedFilterItem[headerPosKey][headerSlug].splice(idx, 1);
+                    }
+                    else {
+                        $scope.appliedFilterItem[headerPosKey][headerSlug].push(filterEntry);
+                    }
+                }
 
         }
 
-        $scope.isFilterItemActive = function(item)
+        $scope.isFilterItemActive = function(itemObj)
         {
-            if($scope.appliedFilterItem.indexOf(item) == -1)
-            {
-                return '';
-            }
-            else {
-                return 'CarBrowser__filters-list-item-selected';
-            }
+
+            var arrIndex;
+            var result = null;
+            var keyPointer = null;
+            var headerSlug = itemObj.g_slug;
+            var filterEntry = itemObj.titleEN;
+            var headerPosKey;
+            var headerMatched;
+
+            for(var key in $scope.appliedFilterItem)
+           {
+                if($scope.appliedFilterItem[key].hasOwnProperty(headerSlug))
+                {
+                    headerMatched = true;
+                    headerPosKey = key;
+
+                    
+                }
+                else {                   
+                    headerMatched = false;
+                }
+
+                if( (headerMatched) && ($scope.appliedFilterItem[headerPosKey][headerSlug].indexOf(filterEntry) != -1))
+                {
+                    return 'CarBrowser__filters-list-item-selected';
+                }
+
+
+
+           }
+
             
+
         }
 
 
 
-        $http.get(targetUrl).then(
+        $http.get(vm.targetUrl).then(
 
 	        function(response) {		
 
@@ -105,11 +224,11 @@
                 
 	        }, function(response) {
 
-        	   console.log('error has occured');
+        	   
 
         	});
 
-	        console.log('exammple controller is loaded');
+	        
 
     });
 
